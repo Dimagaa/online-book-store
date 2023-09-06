fd1ae18c26f247109e9eac52076df7745cdc0e1b
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-09-06 15:44:35 +0300

    Add CustomGlobalExceptionHandler

---
+++ src/main/java/com/app/onlinebookstore/exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.app.onlinebookstore.exception;
+
+import com.app.onlinebookstore.dto.ArgumentNotValidResponse;
+import java.time.LocalDateTime;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        String[] errorMessages = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(this::getErrorMessage)
+                .toArray(String[]::new);
+        ArgumentNotValidResponse responseBody = new ArgumentNotValidResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST,
+                errorMessages);
+        return new ResponseEntity<>(responseBody, headers, status);
+    }
+
+    private String getErrorMessage(ObjectError error) {
+        if (error instanceof FieldError) {
+            String field = ((FieldError) error).getField();
+            return field + " " + error.getDefaultMessage();
+        }
+        return error.getDefaultMessage();
+    }
+}
