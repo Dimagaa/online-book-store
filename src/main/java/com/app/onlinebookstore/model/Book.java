14cb72eea70933d513a1b2a7243e16ff5168cae9
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-09-13 08:55:10 +0300

    Add Category model Create Category table and books-categories table

--- src/main/java/com/app/onlinebookstore/model/Book.java
+++ src/main/java/com/app/onlinebookstore/model/Book.java
@@ -5,9 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import java.util.Set;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.lang3.builder.ToStringExclude;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -32,4 +38,11 @@ public class Book {
     private String coverImage;
     @Column(nullable = false)
     private boolean isDeleted;
+    @ToStringExclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany
+    @JoinTable(name = "books_categories",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 }
