ad66cd114611149c053e581c3640a0b56a9fcbf6
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-09-13 18:36:18 +0300

    Add Pagination, Sorting, and Swagger Implement Security Requirements

--- src/main/java/com/app/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/com/app/onlinebookstore/repository/book/BookRepository.java
@@ -2,6 +2,7 @@ package com.app.onlinebookstore.repository.book;
 
 import com.app.onlinebookstore.model.Book;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -12,4 +13,10 @@ import org.springframework.stereotype.Repository;
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     @Query("FROM Book b LEFT JOIN FETCH b.categories c WHERE c.id = :categoryId")
     List<Book> findAllByCategoryId(Long categoryId, Pageable pageable);
+
+    @Query("FROM Book b LEFT JOIN FETCH b.categories")
+    List<Book> findAllWithCategories(Pageable pageable);
+
+    @Query("FROM Book b LEFT JOIN FETCH b.categories WHERE b.id = :id")
+    Optional<Book> findByIdWithCategories(Long id);
 }
